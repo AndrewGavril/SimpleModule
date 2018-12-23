caeb6a94e1dbaf4034cf60359fd2bb25c5fe24cf
AndrewGavril <gavrilov.andrew1999@yandex.ru>
2018-12-23 19:27:28 +0300

    Adding log with process information

--- hw_module.c
+++ hw_module.c
@@ -1,12 +1,14 @@
 #include <linux/module.h>
-
+#include <linux/sched.h>
+#include <asm/thread_info.h>
 MODULE_LICENSE("GPL");
 
 int init_module(void){
-	printk(KERN_INFO "Hello world!\n");
+	struct task_struct *cur=current;
+	printk(KERN_INFO "State:%lu\nPid:%d\nProcess stack adress:%p\n",cur->state,cur->pid, cur->stack);
 	return 0;
 }
 
 void cleanup_module(void){
-	printk(KERN_INFO "Bie!\n");
+	printk(KERN_INFO "Bye!\n");
 }
